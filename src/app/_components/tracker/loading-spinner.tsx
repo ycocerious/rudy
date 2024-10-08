175fc1c59399eb88ed3f69ded4f187678776efc7
Punarv Dinakar <tranquilblue91@gmail.com>
2024-10-08 18:02:06 +0530

    scroller fixed

--- src/app/_components/tracker/loading-spinner.tsx
+++ src/app/_components/tracker/loading-spinner.tsx
@@ -1,6 +1,6 @@
 export const LoadingSpinner = () => {
   return (
-    <div className="flex h-[127px] items-center justify-center sm:h-[133px] md:h-[144px]">
+    <div className="flex h-[135px] items-center justify-center sm:h-[141px] md:h-[152px]">
       <div className="h-12 w-12 animate-spin rounded-full border-b-2 border-t-2 border-[#5ce1e6]"></div>
     </div>
   );
--- src/app/_components/tracker/scroll-right-wrapper.tsx
+++ src/app/_components/tracker/scroll-right-wrapper.tsx
@@ -34,11 +34,11 @@ export default function ScrollRightWrapper({
         ref={containerRef}
         className="hide-scrollbar custom-scrollbar h-full w-full overflow-x-auto"
       >
-        <div className="pb-2">{children}</div>
+        <div className="pb-4">{children}</div>
       </div>
       <div className="custom:opacity-0 absolute bottom-0 left-0 right-0 h-1 bg-white/10 transition-opacity duration-300">
         <div
-          className="absolute h-full bg-white/40 transition-all duration-300 ease-out"
+          className="absolute h-full bg-white/30 transition-all duration-300 ease-out"
           style={{
             width: "20%",
             left: `${Math.min(Math.max(scrollPercentage, 0), 80)}%`,
