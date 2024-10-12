c6caff20fe1a3b91db5ca073d218fae3b07f7776
Punarv Dinakar <tranquilblue91@gmail.com>
2024-10-12 23:55:36 +0530

    cleanup

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <div className="flex h-full w-full flex-col items-center justify-start bg-[#0e4a4a] pt-10">
+    <div className="flex h-full w-full flex-col items-center justify-start bg-[#0e4a4a] px-4 pt-20">
       <Image
         src="/logo.png"
         alt="logo"
@@ -14,7 +14,7 @@ export default function Page() {
       <h1 className="mb-2 text-2xl text-white">
         Welcome to <span className="text-[#5ce1e6]">Rudy!</span>
       </h1>
-      <p className="text-md mb-10 text-center text-white">
+      <p className="text-md mb-10 px-6 text-center text-white">
         The simplest way to build habits and systems in your life
       </p>
       <SignUp />
