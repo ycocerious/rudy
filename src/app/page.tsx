2588d54a1ff4ee02b572b86e061abdf1c7e2026c
Punarv Dinakar <tranquilblue91@gmail.com>
2025-01-15 03:10:18 +0530

    removed pwa wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,42 @@
+"use client";
+
+import { usePwaDisplayMode } from "@/hooks/usePwaDisplayMode";
 import "@/styles/globals.css";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import LandingPage from "./_components/landing-page";
-import { PwaWrapper } from "./_components/pwa-wrapper";
+import { LoadingSpinner } from "./_components/tracker/loading-spinner";
 
 export default function Home() {
-  return (
-    <PwaWrapper>
-      <LandingPage />
-    </PwaWrapper>
-  );
+  const displayMode = usePwaDisplayMode();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (displayMode === "standalone") {
+      router.replace("/home");
+    }
+  }, [displayMode, router]);
+
+  if (displayMode === null) {
+    return (
+      <div className="flex min-h-screen items-center justify-center">
+        <LoadingSpinner />
+      </div>
+    );
+  } else if (displayMode === "standalone") {
+    return (
+      <div className="flex min-h-screen items-center justify-center bg-[#0e4a4a]">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  return <LandingPage />;
 }
+
+// import "@/styles/globals.css";
+// import ServerPage from "./_components/server-page";
+
+// export default function Home() {
+//   return <ServerPage />;
+// }
