8b3a9ad2e2f3c4afb18214c98970fe671229546f
Punarv Dinakar <tranquilblue91@gmail.com>
2025-01-15 02:47:23 +0530

    updated middleware to not affect manifest json

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -10,7 +10,7 @@ export async function loginWithGoogle() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `${process.env.NEXT_PUBLIC_APP_URL}/auth/callback`,
+      redirectTo: `${process.env.NEXT_PUBLIC_APP_URL}/auth/callback?next=/home`,
     },
   });
 
